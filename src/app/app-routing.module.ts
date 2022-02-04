27319618d5d94de0069c58a04fa256bb4f05e212
Xavier2414 <73123181+Xavier2414@users.noreply.github.com>
2022-02-04 20:09:03 +0100

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './vistas/alumnos/login/login.component';
 import { DashboardComponent } from './vistas/alumnos/dashboard/dashboard.component';
 import { RegistroComponent } from './vistas/alumnos/registro/registro.component';
 import { IdentificacionComponent } from './vistas/identificacion/identificacion.component';
+import { HeaderComponent } from './plantillas/header/header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'identificacion', pathMatch: 'full' },
   { path: 'identificacion', component: IdentificacionComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'registro', component: RegistroComponent }
+  { path: 'registro', component: RegistroComponent },
+  { path: 'header', component: HeaderComponent}
 ];
 
 @NgModule({
