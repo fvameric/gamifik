94c4327cf1e226cba8ca2f346693ff96e517897f
bieelnadal <73020106+bieelnadal@users.noreply.github.com>
2022-02-07 19:31:08 +0100

    header

--- src/app/plantillas/header/header.component.ts
+++ src/app/plantillas/header/header.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+window.addEventListener("scroll", function () {
+  let header = <HTMLElement>document.getElementById("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
