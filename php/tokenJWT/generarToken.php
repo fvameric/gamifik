a59482d1948ffad22a8f400533ac186299fa590b
fvameric <fvameric@gmail.com>
2022-02-18 20:09:10 +0100

    status token

--- php/tokenJWT/generarToken.php
+++ php/tokenJWT/generarToken.php
@@ -14,8 +14,8 @@
   $key = "example_key";
   $iat = time();
   $nbf = $iat + 10;
-  $exp = $nbf + 7200; // 2 horas
-  //$exp = $nbf + 10;
+  //$exp = $nbf + 7200; // 2 horas
+  $exp = $nbf + 10;
   $payload = array(
       "iss" => "http://example.org",
       "aud" => "http://example.com",
@@ -24,22 +24,10 @@
       "exp" => $exp
   );
 
-  /**
-   * IMPORTANT:
-   * You must specify supported algorithms for your application. See
-   * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
-   * for a list of spec-compliant algorithms.
-   */
   $jwt = JWT::encode($payload, $key, 'HS256');
   JWT::$leeway = 60; // $leeway in seconds
   $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
   
   //print_r($decoded);
-
-  /*
-  NOTE: This will now be an object instead of an associative array. To get
-  an associative array, you will need to cast it as such:
-  */
-
   $decoded_array = (array) $decoded;
 ?>
