e2d7526534f6fabb1afa483d7f52c876126a649d
fvameric <fvameric@gmail.com>
2022-04-05 17:41:27 +0200

    dark mode localstorage

--- src/app/vistas/dark-mode/dark-mode.component.ts
+++ src/app/vistas/dark-mode/dark-mode.component.ts
@@ -8,52 +8,53 @@ import { DOCUMENT } from '@angular/common';
 })
 export class DarkModeComponent implements OnInit {
   theme: string = 'light-theme';
-  mostrarSolVisual:boolean = true;
-  mostrarLunaVisual:boolean = false;
-  GuardarDatos:string = "" ;
+  mostrarSolVisual: boolean = true;
+  mostrarLunaVisual: boolean = false;
+  GuardarDatos: string = "";
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2
   ) {
-    
-   }
 
-  ngOnInit() {
-    this.GuardarDatos = JSON.parse(localStorage.getItem('DarkMode') ||'[]');
-    this.theme = this.GuardarDatos;
-    this.initializeTheme(); 
   }
 
-     switchTheme():void {
+  ngOnInit() {
+    this.guardarLocalStorage();
+    this.GuardarDatos = JSON.parse(localStorage.getItem('DarkMode') || '[]');
+    this.theme = this.GuardarDatos;
+    this.initializeTheme();
+  }
+
+  switchTheme(): void {
 
 
-      this.document.body.classList.replace(
-        this.theme,
-        this.theme === 'light-theme'
-          ? (this.theme = 'dark-theme')
-          : (this.theme = 'light-theme')
-    
-      );
+    this.document.body.classList.replace(
+      this.theme,
+      this.theme === 'light-theme'
+        ? (this.theme = 'dark-theme')
+        : (this.theme = 'light-theme')
+
+    );
     document.getElementsByClassName('toggle')[0].classList.toggle('dark');
     document.getElementsByClassName('toggle')[0].classList.toggle('active');
 
-     if (this.mostrarSolVisual==true) {
-       this.mostrarSolVisual=false;
-       this.mostrarLunaVisual=true;
-     }else{
-       this.mostrarSolVisual=true;
-       this.mostrarLunaVisual=false;
-     }
-     this.guardarLocalStorage();
-
+    if (this.mostrarSolVisual == true) {
+      this.mostrarSolVisual = false;
+      this.mostrarLunaVisual = true;
+    } else {
+      this.mostrarSolVisual = true;
+      this.mostrarLunaVisual = false;
     }
+    this.guardarLocalStorage();
 
-    guardarLocalStorage(){
-      localStorage.setItem('DarkMode',JSON.stringify(this.theme) );
-    }
-    
-    initializeTheme = (): void =>
-      this.renderer.addClass(this.document.body, this.theme);
+  }
+
+  guardarLocalStorage() {
+    localStorage.setItem('DarkMode', JSON.stringify(this.theme));
+  }
+
+  initializeTheme = (): void =>
+    this.renderer.addClass(this.document.body, this.theme);
 }
 export type Theme = 'light-theme' | 'dark-theme';
