854e55fc328086f67049676b40591b3180c94e2b
fvameric <fvameric@gmail.com>
2022-04-06 15:01:46 +0200

    test android

---
+++ android/app/src/main/java/gamifik/app/MainActivity.java
@@ -0,0 +1,5 @@
+package gamifik.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gamifik.app',
+  appName: 'gamifik22',
+  webDir: 'dist/gamifik22',
+  bundledWebRuntime: false
+};
+
+export default config;
